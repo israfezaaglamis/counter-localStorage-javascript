323f363dcfc2572b5977b992db314ccc8b9d98ed
İsra Feza Ağlamış <israfezacom@gmail.com>
2022-11-30 13:40:41 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+
+let counter     = localStorage.getItem('counter') ? Number(localStorage.getItem('counter')) : 0
+let counterDOM  = document.querySelector('#counter' )
+let increaseDOM = document.querySelector('#increase') 
+let decreaseDOM = document.querySelector('#decrease') 
+
+
+counterDOM.innerHTML = counter //burada html tarafındaki counter kısmına bağlama işlemi yapıldı
+
+increaseDOM.addEventListener("click", clickEvent) //click eventine fonksiyon ekledik
+decreaseDOM.addEventListener("click", clickEvent) //click eventine fonksiyon ekledik
+
+
+
+function clickEvent() {
+    console.log(typeof(counter))
+    console.log(this.id)
+    this.id == "increase" ? counter +=1 : counter -=1 
+    localStorage.setItem('counter', counter)
+    counterDOM.innerHTML = counter
+
+}
+
+
